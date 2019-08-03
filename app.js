e193911d61eb019395fc8ae182706020b650d6b6
Mark Chen <markchen@Markde-MacBook-Pro.local>
2019-08-03 15:32:17 +0800

    modify static direct

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + 'public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
